5d6cbae326c277ad47842abe23122ebbb88ec84a
ivpprototype <ivpprototype@gmail.com>
2023-09-13 12:07:32 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket connection
+io.on("connection", (socket) => {
+  // console.log("A new client has connected", socket.id);
+  socket.on("move mouse", (data) => {
+    io.emit("move mouse", data);
+  });
+  socket.on("handle click", (data) => {
+    io.emit("handle click", data);
+  });
+  socket.on("touchstart", (data) => {
+    io.emit("touchstart", data);
+  });
+  socket.on("touchmove", (data) => {
+    io.emit("touchmove", data);
+  });
+});
+
+app.use(express.static(path.resolve("./public/")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log("Server listening on port 9000"));
---
+++ public/tv/script.js
@@ -0,0 +1,218 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d"); //get the canvas from html
+
+var colors = ["#FF9AA2", "#FFB7B2", "#FFDAC1", "#E2F0CB", "#B5EAD7", "#C7CEEA"],
+  mouseX = 0,
+  mouseY = 0, //save current mouse/finger position
+  rectangles = [], //array of menu items
+  centerX = 0,
+  centerY = 0, //saves the center position of canvas
+  startX = 0,
+  start = 0, //saves position of mouse/finger where dragging/swiping starts
+  offsetX,
+  offsetY, //offset to center the menu items and move them around, gets in/decreased by dragging
+  oldOffsetX,
+  oldOffsetY, //save old offsets to update current offset
+  scale,
+  i,
+  j, //used for counters
+  x,
+  y, //used for creating the array of rectangles
+  clicked, //for saving the mouse state
+  HORIZONTAL = 10,
+  VERTICAL = 10, //how many rectangles will be on the canvas
+  RECTANGLE_WIDTH = 360,
+  RECTANGLE_WIDTH_HALF = RECTANGLE_WIDTH / 2,
+  RECTANGLE_HEIGHT = 180, //size of rectangles
+  RECTANGLE_HEIGHT_HALF = RECTANGLE_HEIGHT / 2,
+  PADDINGX = 10,
+  PADDINGY = 20, //the gap between rectangles
+  SCALE_FACTOR = 900; //small number = icons get small faster, smaller number = icons get small slowly
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight; //set canvas to full size of the window
+
+offsetX =
+  (canvas.width - RECTANGLE_WIDTH * HORIZONTAL - PADDINGX * (HORIZONTAL - 1)) /
+  2; //center the rectangles horizontally
+offsetY =
+  (canvas.height - RECTANGLE_HEIGHT * VERTICAL - PADDINGY * (VERTICAL - 1)) / 2; //center the rectangles vertically
+
+centerX = canvas.width / 2;
+centerY = canvas.height / 2;
+
+x = 0;
+y = 0;
+
+for (i = 0; i < VERTICAL; i++) {
+  for (j = 0; j < HORIZONTAL; j++) {
+    var randomColor = colors[Math.round(Math.random() * (colors.length - 1))]; //generating a random color for the menu rectangle
+
+    rectangles.push({ x: x, y: y, color: randomColor }); //add rectangle with x and y coordinates and color to the array
+    x += RECTANGLE_WIDTH + PADDINGX; //increase x for the next rectangle
+  }
+
+  if (i % 2 == 0) {
+    //x = PADDINGX / 2 + RECTANGLE_WIDTH; //if its the second, fourth, sixth etc. row then move the row to right
+    x = RECTANGLE_WIDTH / 2;
+  } else {
+    x = 0;
+  }
+
+  y += RECTANGLE_HEIGHT + PADDINGY; //increase y for the next rectangle row
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); //clear the canvas
+
+  ctx.save();
+
+  ctx.translate(offsetX, offsetY);
+
+  for (i = 0; i < rectangles.length; i++) {
+    ctx.save();
+    scale = getDistance(rectangles[i]);
+    //ctx.translate(rectangles[i].x + RECTANGLE_WIDTH / 2, rectangles[i].y + RECTANGLE_HEIGHT / 2); // translate to the center of the rectangle
+    //ctx.scale(scale, scale);
+
+    // ctx.translate(rectangles[i].x, rectangles[i].y);
+    // ctx.translate(RECTANGLE_WIDTH_HALF, RECTANGLE_HEIGHT_HALF);
+    // ctx.scale(scale, scale);
+    // ctx.translate(-RECTANGLE_WIDTH_HALF, -RECTANGLE_HEIGHT_HALF / 2);
+    // ctx.translate(RECTANGLE_WIDTH_HALF, 0);
+
+    ctx.translate(rectangles[i].x, rectangles[i].y);
+    // ctx.translate(RECTANGLE_WIDTH_HALF, RECTANGLE_HEIGHT_HALF);
+    ctx.scale(scale, scale);
+    // ctx.translate(-RECTANGLE_WIDTH_HALF, -RECTANGLE_HEIGHT_HALF / 2);
+
+    ctx.fillStyle = rectangles[i].color;
+    ctx.fillRect(
+      -RECTANGLE_WIDTH / 2,
+      -RECTANGLE_HEIGHT / 2,
+      RECTANGLE_WIDTH,
+      RECTANGLE_HEIGHT
+    ); // draw rectangle centered at (0, 0)
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#000";
+    //ctx.fillText (i, 50,0 + (i * 20));
+    ctx.textAlign = "center";
+    ctx.fillText(i, 0, 0);
+    ctx.restore();
+  }
+
+  ctx.restore();
+  requestAnimationFrame(draw);
+}
+
+draw();
+
+function getDistance(rectangle) {
+  var dx, dy, dist;
+  dx = rectangle.x - centerX + offsetX;
+  dy = rectangle.y - centerY + offsetY;
+  dist = Math.sqrt(dx * dx + dy * dy);
+  scale = 1.2 - dist / SCALE_FACTOR;
+
+  scale = scale > 0 ? scale : 0.5;
+
+  return scale;
+}
+
+window.addEventListener("touchstart", handleTouch);
+
+function handleTouch(e) {
+  window.addEventListener("touchmove", handleSwipe);
+  // startX = e.touches[0].clientX;
+  // startY = e.touches[0].clientY;
+  startX = e.clientX;
+  startY = e.clientY;
+  oldOffsetX = offsetX;
+  oldOffsetY = offsetY;
+}
+
+function handleSwipe(e) {
+  // mouseX = e.changedTouches[0].clientX;
+  // mouseY = e.changedTouches[0].clientY;
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  offsetX = oldOffsetX + mouseX - startX;
+  offsetY = oldOffsetY + mouseY - startY;
+}
+
+window.addEventListener("touchend", () => {
+  window.removeEventListener("touchmove", handleSwipe);
+});
+
+window.addEventListener("mousedown", handleClick);
+
+// function handleClick(e) {
+//   window.addEventListener("mousemove", handleMouse);
+//   window.addEventListener("mouseup", handleRelease);
+//   startX = e.clientX;
+//   startY = e.clientY;
+//   oldOffsetX = offsetX;
+//   oldOffsetY = offsetY;
+//   canvas.style.cursor = "grabbing";
+// }
+
+// function handleMouse(e) {
+//   mouseX = e.clientX;
+//   mouseY = e.clientY;
+//   offsetX = oldOffsetX + mouseX - startX;
+//   offsetY = oldOffsetY + mouseY - startY;
+// }
+
+function handleClick(e) {
+  // window.addEventListener("mousemove", handleMouse);
+  // window.addEventListener("mouseup", handleRelease);
+  //console.log("handle click");
+  startX = e.clientX;
+  startY = e.clientY;
+  oldOffsetX = offsetX;
+  oldOffsetY = offsetY;
+  // canvas.style.cursor = "grabbing";
+}
+
+function handleMouse(e) {
+  // console.log("handle mouse");
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  offsetX = oldOffsetX + mouseX - startX;
+  offsetY = oldOffsetY + mouseY - startY;
+}
+
+function handleRelease() {
+  window.removeEventListener("mouseup", handleRelease);
+  window.removeEventListener("mousemove", handleMouse);
+  canvas.style.cursor = "grab";
+}
+
+window.addEventListener("resize", () => {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  centerX = canvas.width / 2;
+  centerY = canvas.height / 2;
+});
+
+var socket = io();
+
+socket.on("move mouse", (data) => {
+  //received message
+  handleMouse(data);
+});
+
+socket.on("handle click", (data) => {
+  //received message
+  handleClick(data);
+});
+
+socket.on("touchmove", (data) => {
+  //received message
+  handleSwipe(data);
+});
+
+socket.on("touchstart", (data) => {
+  //received message
+  handleTouch(data);
+});
